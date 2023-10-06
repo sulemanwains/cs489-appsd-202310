471568323b3ea56d9f1737ce9442ad4e0b4a42ec
“Suleman <suleman.venus2@gmail.com>
2023-10-06 17:46:29 -0500

    Lab 3_4 done

---
+++ Lab3_4/Task1/eLibrary/src/main/java/edu/miu/cs489/demowebapps/elibrary/ELibraryApplication.java
@@ -0,0 +1,13 @@
+package edu.miu.cs489.demowebapps.elibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ELibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ELibraryApplication.class, args);
+	}
+
+}
